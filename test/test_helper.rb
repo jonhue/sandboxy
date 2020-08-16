874681423aaaaa53a2c1453e4511283254aee6c6
Jonas Hübotter <jonas.huebotter@gmail.com>
2020-08-16 14:27:22 +0200

    address rubocop offenses

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,10 +10,10 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('dummy30/config/environment.rb', __dir__)
 require 'rails/test_help'
 
-ActiveRecord::Base.logger = Logger.new File.dirname(__FILE__) + '/debug.log'
+ActiveRecord::Base.logger = Logger.new "#{File.dirname(__FILE__)}/debug.log"
 ActiveRecord::Migration.verbose = false
 
-load File.dirname(__FILE__) + '/schema.rb'
+load "#{File.dirname(__FILE__)}/schema.rb"
 
 require 'shoulda'
 require 'factory_bot'
